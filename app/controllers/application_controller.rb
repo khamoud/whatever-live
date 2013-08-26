098946428903b7a29498ede91f2b6d53c9bc28ae
Kris Hamoud <khamoud617@gmail.com>
2013-08-26 13:28:57 -0700

    login fix 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   def after_sign_in_path_for(resource)
   	if current_company.user_level == 9
-  		company_campaigns_path(Company.find(first).campaigns(1))
+  		company_campaigns_path(Company.find(first))
   	else
     	company_campaigns_path(current_company)
     end
