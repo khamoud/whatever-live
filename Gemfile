480d82c2e99771bc69db1d6c4c8695dd21c3391f
Kris Hamoud <khamoud617@gmail.com>
2013-08-26 13:16:27 -0700

    ruby version

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.0.0'
+ruby '1.9.3'
 gem 'foreman'
 gem 'rails', '3.2.13'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def after_sign_in_path_for(resource)
-    company_campaigns_path(current_company)
+  	if current_company.user_level == 9
+  		company_campaigns_path(Company.find(1))
+  	else
+    	company_campaigns_path(current_company)
+    end
   end
   def allow_edit
   	unless current_company && current_company.user_level == 9
