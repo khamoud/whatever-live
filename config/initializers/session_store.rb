b72a8064a7e074e6970466c71e852be0a30ac63e
Kris Hamoud <khamoud617@gmail.com>
2013-09-12 11:37:49 -0700

    fuck...

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-#SpreeDash::Application.config.session_store :cookie_store, key: '_spree_dash_session'
-if Rails.env.production?
-SpreeDash::Application.config.session_store :cookie_store, {:key => '_cookie_name', :domain => :all}
-else
-SpreeDash::Application.config.session_store :cookie_store, :key => '_cookie_name'
-end
+Whatever::Application.config.session_store :cookie_store, key: '_spree_dash_session'
+
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
